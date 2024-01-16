a0260d0d2f7f624ee2fdb0a2e9c29c144df9f66b
joaovdbrites <jvd.brites@gmail.com>
2024-01-16 10:35:47 -0400

    2# test

--- Program.cs
+++ Program.cs
@@ -1,8 +1,35 @@
-﻿
-int numero = 5;
+﻿string opcao;
 
-Console.WriteLine($"{numero} x 1 = {numero * 1}");
+while (true)
+{
+    Console.WriteLine ("Digite sua opcao");
+    Console.WriteLine ("1 - Cadastrar cliente");
+    Console.WriteLine ("2 - Buscar cliente");
+    Console.WriteLine ("3 - Apagar cliente");
+    Console.WriteLine ("4 - Encerrar cliente");
 
-for (int contador = 0; contador <= 10; contador++){
-    Console.WriteLine($"{numero} x {contador} = {numero * contador}");
+    opcao = Console.ReadLine();
+
+    switch (opcao){
+        case "1":
+            Console.WriteLine ("Cadastro Cliente");
+            break;
+    
+        case "2":
+            Console.WriteLine ("Buscar Cliente");
+            break;
+        
+        case "3":
+            Console.WriteLine ("Apagar Cliente");
+            break;
+        
+        case "4 -  encerra cliente":
+            Console.WriteLine ("Encerrar Cliente");
+            Environment.Exit(0);
+            break;
+        default:
+            Console.WriteLine ("Opção errada");
+            break;
+    }
+    
 }
